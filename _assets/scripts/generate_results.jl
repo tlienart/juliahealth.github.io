8b447fe8638e62be1f109d97df37d52845c1f346
Dilum Aluthge <dilum@aluthge.com>
2020-05-10 13:24:02 +0000

    Rename some variables and reorganize some code

--- _assets/scripts/generate_results.jl
+++ _assets/scripts/generate_results.jl
@@ -1,11 +1,3 @@
-# Parent file to run all scripts which may generate
-# some output that you want to display on the website.
-# this can be used as a tester to check that all the code
-# on your website runs properly.
-
-file = abspath(@__FILE__)
-dir = abspath(dirname(file))
-
 """
     genplain(s)
 
@@ -19,5 +11,10 @@ function genplain(s::String)
     end
 end
 
-include(abspath(dir, "package_list.jl"))
-generate_package_list("JuliaHealth", abspath(dir, "package_list.md"))
+const FILE = abspath(@__FILE__) # _assets/scripts/generate_results.jl
+const DIR = abspath(dirname(FILE)) # _assets/scripts/
+
+const package_list_jl_file = abspath(DIR, "package_list.jl") # _assets/scripts/package_list.jl
+const package_list_md_file = abspath(DIR, "package_list.md") # _assets/scripts/package_list.md
+include(package_list_jl_file)
+generate_package_list("JuliaHealth", package_list_md_file)
