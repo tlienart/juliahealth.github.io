c56fbdac10c2f096e1c73a63ae5d3d26cfb2beff
Dilum Aluthge <dilum@aluthge.com>
2020-05-23 22:21:53 -0400

    Reorder some lines of code

--- _assets/scripts/generate_results.jl
+++ _assets/scripts/generate_results.jl
@@ -15,8 +15,8 @@ const FILE = abspath(@__FILE__) # _assets/scripts/generate_results.jl
 const DIR = abspath(dirname(FILE)) # _assets/scripts/
 
 const package_list_jl_file = abspath(DIR, "package_list.jl") # _assets/scripts/package_list.jl
-const package_list_md_file = abspath(DIR, "package_list.md") # _assets/scripts/package_list.md
 include(package_list_jl_file)
+const package_list_md_file = abspath(DIR, "package_list.md") # _assets/scripts/package_list.md
 const packages_to_exclude = String[]
 generate_package_list("JuliaHealth",
                       package_list_md_file;
